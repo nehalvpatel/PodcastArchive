1ddc1e2ca298cce3b211b972f176a4eb60883c10
Nehal Patel <nehal@itspatel.com>
2013-12-16 22:38:40 -0600

    Forgot to upload config.php

---
+++ config.php
@@ -0,0 +1,24 @@
+<?php
+
+	require_once("mysql.php");
+	require_once("class.podcast.php");
+	require_once("class.episode.php");
+	
+	$Podcast = new Podcast($con);
+	
+	$Podcast->setName("Painkiller Already");
+	$Podcast->setDescription("Three gamers discuss games, current events, and tell a few stories.");
+	$Podcast->setPrefix("PKA");
+	
+	$Podcast->setTable("Episodes");
+	$Podcast->setBlubrry("painkilleralready");
+	$Podcast->setFeedburner("Painkiller_Already");
+	$Podcast->setSubreddit("PKA");
+	$Podcast->setItunes("692564838");
+	
+	$Podcast->setCollection("painkilleralready");
+	
+	$Podcast->setAuthorName("Nehal Patel");
+	$Podcast->setAuthorEmail("nehal@itspatel.com");
+
+?>
