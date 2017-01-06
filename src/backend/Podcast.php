bfb399f6d74113f5e9c1640e68b081b1d4a30663
Nehal Patel <nehalvpatels+github@gmail.com>
2017-01-06 17:24:07 -0600

    Excluded deleted timestamps from search results

--- src/backend/Podcast.php
+++ src/backend/Podcast.php
@@ -245,20 +245,22 @@ class Podcast
 	{
 		$search_results = array();
 		if (!empty($query)) {
-			$search_query = $this->_connection->prepare("SELECT `ID`, `Episode`, `Timestamp`, `Value` FROM `timestamps` WHERE REPLACE(`Value`, :Replace, '') LIKE :Value");
+			$search_query = $this->_connection->prepare("SELECT `ID`, `Episode`, `Timestamp`, `Value`, `Deleted` FROM `timestamps` WHERE REPLACE(`Value`, :Replace, '') LIKE :Value");
 			$search_query->bindValue(":Replace", "'");
 			$search_query->bindValue(":Value", "%" . str_replace("'", "", trim($query) . "%"));
 			$search_query->execute();
 			$search_query_results = $search_query->fetchAll();
 
 			foreach ($search_query_results as $result) {
-				$timestamp_data = array();
-				$timestamp_data["ID"] = (int)$result["ID"];
-				$timestamp_data["Timestamp"] = $result["Timestamp"];
-				$timestamp_data["Value"] = $result["Value"];
-				$timestamp_data["HMS"] = Utilities::convertToHMS($result["Timestamp"]);
-				
-				$search_results[$result["Episode"]][] = $timestamp_data;
+				if ($result["Deleted"] == 0) {
+					$timestamp_data = array();
+					$timestamp_data["ID"] = (int)$result["ID"];
+					$timestamp_data["Timestamp"] = $result["Timestamp"];
+					$timestamp_data["Value"] = $result["Value"];
+					$timestamp_data["HMS"] = Utilities::convertToHMS($result["Timestamp"]);
+					
+					$search_results[$result["Episode"]][] = $timestamp_data;
+				}
 			}
 		} else {
 			$search_query = $this->_connection->prepare("SELECT * FROM `episodes`");
